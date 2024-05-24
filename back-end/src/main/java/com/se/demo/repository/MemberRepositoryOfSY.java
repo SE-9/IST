e7026ba2884c795eab4f98087b84a5fe83adeba0
siyeon02 <101983426+siyeon02@users.noreply.github.com>
2024-05-24 13:32:16 +0900

    Update MemberRepositoryOfSY.java

--- back-end/src/main/java/com/se/demo/repository/MemberRepositoryOfSY.java
+++ back-end/src/main/java/com/se/demo/repository/MemberRepositoryOfSY.java
@@ -1,4 +1,9 @@
-package com.se.demo.repository;
 
-public interface MemberRepositoryOfSY {
+
+import java.lang.reflect.Member;
+
+import com.se.demo.entity.MemberEntityOfSY;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepositoryOfSY extends JpaRepository<MemberEntityOfSY, Integer> {
 }
