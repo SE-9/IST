f92e029ea3eea6aff84cdb18a65385004b6044a8
siyeon02 <101983426+siyeon02@users.noreply.github.com>
2024-05-24 13:31:16 +0900

    Update MemberDTOOfSY.java

--- back-end/src/main/java/com/se/demo/dto/MemberDTOOfSY.java
+++ back-end/src/main/java/com/se/demo/dto/MemberDTOOfSY.java
@@ -1,4 +1,50 @@
 package com.se.demo.dto;
 
+import com.se.demo.entity.MemberEntityOfSY;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.antlr.v4.runtime.misc.NotNull;
+
 public class MemberDTOOfSY {
+
+    public String getNickname() {
+        return null;
+    }
+
+    public String getPassword() {
+        return null;
+    }
+
+    public String getId() {
+        return null;
+    }
+
+    @Getter
+    @Setter
+    @Data
+
+    public static class MemberDTO {
+
+        @NotBlank(message = "아이디는 필수 입력 값입니다.")
+        private Integer id;
+
+        @NotBlank(message = "닉네임은 필수 입력 값입니다.")
+        private String nickname;
+
+        @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+        private String password;
+
+
+
+        public MemberEntityOfSY toEntity() {
+            return MemberEntityOfSY.builder()
+
+                    .id(id)
+                    .nickname(nickname)
+                    .password(password)
+                    .build();
+        }
+
+    }
 }
